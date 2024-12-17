183ca05124607d2a3a5d2eef88922eaea8507273
Megha Byahatti <quic_mbyahatt@quicinc.com>
2024-12-17 15:24:30 +0530

    mmrm: driver: Add mmrm support for tuna variant
    Add mmrm support for tuna variant
    
    Change-Id: Id7bc402a7fbe1f0d0b86b0af165b8a474ba0ab07
    Signed-off-by: Megha Byahatti <quic_mbyahatt@quicinc.com>

--- driver/src/mmrm_internal.c
+++ driver/src/mmrm_internal.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2024-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/types.h>
@@ -112,6 +112,29 @@ static struct mmrm_throttle_clients_data common_pt_throttle_clients_data_canoe[]
 	},
 };
 
+static struct mmrm_throttle_clients_data common_pt_throttle_clients_data_tuna[] = {
+	{
+		.domain = MMRM_CLIENT_DOMAIN_DISPLAY,
+		.id = 0x42,
+	},
+	{
+		.domain = MMRM_CLIENT_DOMAIN_VIDEO,
+		.id = 0x03,
+	},
+	{
+		.domain = MMRM_CLIENT_DOMAIN_CAMERA,
+		.id = 0x45,
+	},
+	{
+		.domain = MMRM_CLIENT_DOMAIN_CVP,
+		.id = 0x03,
+	},
+	{
+		.domain = MMRM_CLIENT_DOMAIN_CAMERA,
+		.id = 0x4F,
+	},
+};
+
 static struct mmrm_platform_data commom_pt_platform_data = {
 	.common_data = common_pt_data,
 	.common_data_length = ARRAY_SIZE(common_pt_data),
@@ -140,6 +163,13 @@ static struct mmrm_platform_data commom_pt_platform_data_canoe = {
 	.throttle_clk_clients_data_length = ARRAY_SIZE(common_pt_throttle_clients_data_canoe),
 };
 
+static struct mmrm_platform_data commom_pt_platform_data_tuna = {
+	.common_data = common_pt_data,
+	.common_data_length = ARRAY_SIZE(common_pt_data),
+	.throttle_clk_clients_data = common_pt_throttle_clients_data_tuna,
+	.throttle_clk_clients_data_length = ARRAY_SIZE(common_pt_throttle_clients_data_tuna),
+};
+
 static const struct of_device_id mmrm_dt_match[] = {
 	{
 		.compatible = "qcom,waipio-mmrm",
@@ -160,6 +190,10 @@ static const struct of_device_id mmrm_dt_match[] = {
 	{
 		.compatible = "qcom,canoe-mmrm",
 		.data = &commom_pt_platform_data_canoe,
+	},
+	{
+		.compatible = "qcom,tuna-mmrm",
+		.data = &commom_pt_platform_data_tuna,
 	},
 	{},
 };
